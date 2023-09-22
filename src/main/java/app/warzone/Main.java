54aa01cf7f0dcbb6598c66fa33539382eb43ed49
bhsrampage <burhanuddinsavli@gmail.com>
2023-09-22 00:45:39 -0400

    Jdoc initialization

--- src/main/java/app/warzone/Main.java
+++ src/main/java/app/warzone/Main.java
@@ -2,7 +2,18 @@ package app.warzone;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+
+/**
+ * This is the main runner class which is the root of the project
+ * @author Burhanuddin Savliwala
+ */
 public class Main {
+    /**
+     *
+     * @param firstNum The first number
+     * @param secondNum The second number
+     * @return The sum of the two integers
+     */
     public static int calcSum(int firstNum , int secondNum) {
         return firstNum + secondNum;
     }
