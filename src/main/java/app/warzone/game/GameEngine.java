ce18d47784102ca652c84ab258f473526bed13bc
bhsrampage <burhanuddinsavli@gmail.com>
2023-10-02 13:53:23 -0400

    Added javadoc comments + remaining functionalities

--- src/main/java/app/warzone/game/GameEngine.java
+++ src/main/java/app/warzone/game/GameEngine.java
@@ -1,6 +1,10 @@
 package app.warzone.game;
 
 import app.warzone.Main.Phase;
+import app.warzone.map.MapUtils;
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -9,7 +13,7 @@ import java.util.Scanner;
  */
 public class GameEngine {
     Scanner SCAN;
-    Phase d_currPhase;
+    Phase d_currPhase = null;
 
     public GameEngine(){
         SCAN = new Scanner(System.in);
@@ -19,19 +23,45 @@ public class GameEngine {
         return d_currPhase;
     }
 
+    void listenMapCommands(){
+        System.out.println("**Map Editor**\n");
+        MapUtils l_targetMapUtil= new MapUtils();
+        boolean l_isEditing = true;
+
+        while (l_isEditing){
+            String l_userInput = SCAN.nextLine();
+            String[] l_cmdTokens = l_userInput.split(" ");
+            List<String> arguments = Arrays.asList(Arrays.copyOfRange(l_cmdTokens,1,l_cmdTokens.length));
+
+            switch (l_cmdTokens[0]){
+                case "editcontinent": l_targetMapUtil.editContinent(arguments); break;
+                case "editcountry": l_targetMapUtil.editCountry(arguments); break;
+                case "editneighbor": l_targetMapUtil.editNeighbour(arguments); break;
+                case "savemap": l_targetMapUtil.saveMap(); l_isEditing = false; break;
+                case "editmap": l_targetMapUtil.editMap(arguments); break;
+                case "validatemap": l_targetMapUtil.validateMap(); break;
+                case "showmap":l_targetMapUtil.showMap();break;
+                case "exit": l_isEditing = false; break;
+                default: System.out.println("Invalid Command try again..");
+            }
+        }
+
+
+    }
+
     public void initialize() {
         System.out.println("Welcome to Risk (Warzone) by U6 build1");
-        int choice;
+        String choice;
         boolean playing = true;
         while (playing){
             System.out.println("1. Map Editor\n2. Play Game\nEnter your choice:- ");
-            choice = SCAN.nextInt();
+            choice = SCAN.nextLine();
             switch (choice){
-               case 1: d_currPhase = Phase.MAP_ACTIONS; break;
-               case 2: d_currPhase = Phase.GAMEPLAY; break;
+               case "1": d_currPhase = Phase.MAP_ACTIONS; listenMapCommands(); break;
+               case "2": d_currPhase = Phase.GAMEPLAY; break;
                default: playing = false; break;
             }
-
+        System.out.println("Quiting...");
         }
 
     }
