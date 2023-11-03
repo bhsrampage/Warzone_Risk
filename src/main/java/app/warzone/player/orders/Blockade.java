0dff8d673bfd85f3018ccbcf5150095aededdf65
Balaji Moturu <balajimoturu143@gmail.com>
2023-11-03 17:57:02 -0400

    Created an Initial Blockade Class.

---
+++ src/main/java/app/warzone/player/orders/Blockade.java
@@ -0,0 +1,16 @@
+package app.warzone.player.orders;
+
+import app.warzone.map.Country;
+import app.warzone.player.Player;
+
+public class Blockade extends Order {
+
+    Player d_player;
+    Country d_country;
+    String d_countryName;
+
+    @Override
+    public void execute() {
+
+    }
+}
