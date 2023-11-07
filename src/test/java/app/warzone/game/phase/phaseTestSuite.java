e4cd4f6a2f10d14933a17434f7cea6ee78844744
anuprashanth.10@gmail.com <Bloated@123>
2023-11-06 21:18:43 -0500

    Test Suite

---
+++ src/test/java/app/warzone/game/phase/phaseTestSuite.java
@@ -0,0 +1,10 @@
+package app.warzone.game.phase;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages("app.warzone.game.phase")
+public class phaseTestSuite {
+    // This class doesn't need any code in it.
+}
