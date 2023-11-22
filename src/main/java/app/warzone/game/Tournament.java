f145dc2a126d19cb0b83f150744b670b9cf1788a
Balaji Moturu <balajimoturu143@gmail.com>
2023-11-21 21:02:45 -0500

    Added Tournament Class

---
+++ src/main/java/app/warzone/game/Tournament.java
@@ -0,0 +1,21 @@
+package app.warzone.game;
+
+
+import java.util.ArrayList;
+
+public class Tournament {
+
+    static ArrayList<String> d_mapsList;
+    static ArrayList<String> d_playerStrategiesList;
+
+    static int d_gamesCount;
+    static int d_maximumTurns;
+
+    Tournament() {
+        d_mapsList = new ArrayList<>();
+        d_playerStrategiesList = new ArrayList<>();
+    }
+
+
+
+}
