9f504d7426501a0562543a57747004b8816bc7f4
apurba-das <contactapurbadas@gmail.com>
2023-11-23 21:46:39 -0500

    Made changes to benevolent class

--- src/main/java/app/warzone/player/strategy/BenevolentStrategy.java
+++ src/main/java/app/warzone/player/strategy/BenevolentStrategy.java
@@ -1,64 +1,124 @@
 package app.warzone.player.strategy;
 
+import java.util.*;
+
+import app.warzone.map.Country;
 import app.warzone.player.Player;
+import app.warzone.player.orders.Advance;
+import app.warzone.player.orders.Airlift;
 import app.warzone.player.orders.Deploy;
 import app.warzone.player.orders.Order;
-import app.warzone.map.Country;
 
 /**
- * The BenevolentStrategy class represents a strategy for a benevolent player in the Warzone game.
- * Benevolent players focus on protecting their weak countries and avoid attacking enemy territories.
+ * Concrete strategy class of Benevolent player. A benevolent computer player
+ * strategy that focuses on protecting its weak countries (deploys on its
+ * weakest country, never attacks, then moves its armies in order to reinforce
+ * its weaker country).
  */
 public class BenevolentStrategy extends PlayerStrategy {
-
-    /**
-     * Constructor for the BenevolentStrategy class.
-     *
-     * @param p_player The player using this strategy.
-     */
     public BenevolentStrategy(Player p_player) {
         super(p_player);
     }
+    public String getStrategyName() {
+        return "benevolent";
+    }
 
     /**
-     * Create an order based on the benevolent strategy.
-     * Benevolent players deploy or advance armies on their weakest countries.
-     *
-     * @return The order created based on the strategy.
+     * This function creates orders keeping in mind how the aggressive player plays.
+     * It uses a switch case to create any random order (move armies, deploy armies,
+     * advance armies, cards) deploys on its weakest country never attacks moves its
+     * armies in order to reinforce its weaker country
      */
     @Override
     public Order createOrder() {
-        // Check if there are weak countries owned by the player
-        if (!d_targetPlayer.d_holdingCountries.isEmpty()) {
-            // Find the weakest country (lowest army count)
-            int minArmyCount = Integer.MAX_VALUE;
-            Country weakestCountry = null;
-
-            for (Country country : d_targetPlayer.d_holdingCountries) {
-                if (country.getCurrentArmyCount() < minArmyCount) {
-                    minArmyCount = country.getCurrentArmyCount();
-                    weakestCountry = country;
+        int randomOrder = new Random().nextInt(3) + 1;
+        switch (randomOrder) {
+            case 1:
+                return deployOnWeakestCountry();
+            case 2:
+                return advanceToWeakestCountry();
+            case 3:
+                if (d_targetPlayer.d_holdingCards.contains("Airlift")) {
+                    return airliftToWeakestCountry();
                 }
-            }
-
-            if (weakestCountry != null) {
-                // Deploy armies to the weakest country
-                int deployArmyCount = Math.max(1, d_targetPlayer.d_currentArmyCount / 2);
-                return new Deploy(d_targetPlayer, deployArmyCount, weakestCountry);
-            }
+                break;
+            default:
+                break;
         }
-
-        // If no weak countries found, return null
         return null;
     }
 
     /**
-     * Get the name of the benevolent strategy.
-     *
-     * @return The name of the strategy.
+     * Deploy armies on the weakest country owned by the player.
      */
-    @Override
-    public String getStrategyName() {
-        return "Benevolent";
+    private Order deployOnWeakestCountry() {
+        Country weakestCountry = getWeakestCountry();
+        if (weakestCountry != null) {
+            int d_deployArmyCount = new Random().nextInt(d_targetPlayer.d_currentArmyCount) + 1;
+            return new Deploy(d_targetPlayer, d_deployArmyCount, weakestCountry);
+        }
+        return null;
+    }
+
+    /**
+     * Airlift armies to the weakest country owned by the player.
+     *
+     * @return The airlift order.
+     */
+    private Order airliftToWeakestCountry() {
+        Country l_weakestCountry = getWeakestCountry();
+        if (l_weakestCountry != null) {
+            Country l_strongNeighbor = getStrongNeighbor(l_weakestCountry);
+            if (l_strongNeighbor != null) {
+                int d_airliftArmyCount = new Random().nextInt(l_strongNeighbor.getCurrentArmyCount()) + 1;
+                return new Airlift(d_targetPlayer, l_strongNeighbor, l_weakestCountry, d_airliftArmyCount);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Advance armies to reinforce the weakest country.
+     */
+    private Order advanceToWeakestCountry() {
+        Country l_weakestCountry = getWeakestCountry();
+        if (l_weakestCountry != null) {
+            Country l_strongNeighbor = getStrongNeighbor(l_weakestCountry);
+            if(l_strongNeighbor != null) {
+                int d_advanceArmyCount = new Random().nextInt(l_strongNeighbor.getCurrentArmyCount()) + 1;
+                return new Advance(d_targetPlayer, l_strongNeighbor, l_weakestCountry, d_advanceArmyCount);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Get the weakest country owned by the player.
+     *
+     * @return Weakest country or null if no countries owned
+     */
+    private Country getWeakestCountry() {
+        List<Country> playerCountries = new ArrayList<>(d_targetPlayer.d_holdingCountries);
+        if (!playerCountries.isEmpty()) {
+            // Sort countries based on the number of armies in ascending order
+            playerCountries.sort(Comparator.comparingInt(Country::getCurrentArmyCount));
+            return playerCountries.get(0); // The first country is the weakest
+        }
+        return null;
+    }
+
+    /**
+     * Get a strong neighbor of a given country.
+     *
+     * @param p_country The country to find a strong neighbor for
+     * @return Strong neighbor or null if no strong neighbors
+     */
+    private Country getStrongNeighbor(Country p_country) {
+        for (Country l_neighbor : p_country.getNeighbouringCountries()) {
+            if (!l_neighbor.equals(p_country) && l_neighbor.getCurrentArmyCount() > 1) {
+                return l_neighbor;
+            }
+        }
+        return null;
     }
 }
